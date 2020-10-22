e5d700bd3d33f2a776c295d8ccfeeb8185bc0f30
lixingDEVIL <1379470308@qq.com>
2020-10-22 22:07:36 +0800

    new

--- build/precache-manifest.c5f3f3424962d357cc7fc0cb6ccb0377.js
+++ build/precache-manifest.c5f3f3424962d357cc7fc0cb6ccb0377.js
@@ -29,74 +29,74 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "e251b3848383219b5fb5174db3e2bea3",
-    "url": "./static/media/chuanglian.e251b384.jpg"
+    "url": "./static/media/chuanglian.jpg"
   },
   {
     "revision": "57a32bcbc6de8a683251476afcd9bb9d",
-    "url": "./static/media/img10.57a32bcb.jpg"
+    "url": "./static/media/img10.jpg"
   },
   {
     "revision": "ee36e0727295130e87608e3ad3a57196",
-    "url": "./static/media/img2.ee36e072.jpg"
+    "url": "./static/media/img2.jpg"
   },
   {
     "revision": "d8b1a9c474b5402cc041c817f4ec170d",
-    "url": "./static/media/img4.d8b1a9c4.jpg"
+    "url": "./static/media/img4.jpg"
   },
   {
     "revision": "cd70a11db20017c955e1d2348fdc48bc",
-    "url": "./static/media/img5.cd70a11d.jpg"
+    "url": "./static/media/img5.jpg"
   },
   {
     "revision": "f5feac5a4dc0fd198fe3f44f282545ac",
-    "url": "./static/media/img6.f5feac5a.jpg"
+    "url": "./static/media/img6.jpg"
   },
   {
     "revision": "cd0930caa46a354efb11e27f1cb30fd9",
-    "url": "./static/media/img7.cd0930ca.jpg"
+    "url": "./static/media/img7.jpg"
   },
   {
     "revision": "5ec76106c2bfa5b369953813620bebb7",
-    "url": "./static/media/img8.5ec76106.jpg"
+    "url": "./static/media/img8.jpg"
   },
   {
     "revision": "6590ea368e5e043da4305e8a05f5835b",
-    "url": "./static/media/img9.6590ea36.jpg"
+    "url": "./static/media/img9.jpg"
   },
   {
     "revision": "71214b2b44ab8ae1314f0f89701366bd",
-    "url": "./static/media/lunbo1.71214b2b.jpg"
+    "url": "./static/media/lunbo1.jpg"
   },
   {
     "revision": "fb6da3f5586e6e031a175a454ccfc324",
-    "url": "./static/media/lunbo2.fb6da3f5.jpg"
+    "url": "./static/media/lunbo2.jpg"
   },
   {
     "revision": "ef96e9081bef1d034f065216e5673fc4",
-    "url": "./static/media/lunbo3.ef96e908.jpg"
+    "url": "./static/media/lunbo3.jpg"
   },
   {
     "revision": "86c837655808ac1e97fd778c0de398e4",
-    "url": "./static/media/lunbo4.86c83765.jpg"
+    "url": "./static/media/lunbo4.jpg"
   },
   {
     "revision": "495824958cdecdcd5f17589056960791",
-    "url": "./static/media/lunbo5.49582495.jpg"
+    "url": "./static/media/lunbo5.jpg"
   },
   {
     "revision": "a66e861692ae96109cb5c2cad4759316",
-    "url": "./static/media/lunbo6.a66e8616.jpg"
+    "url": "./static/media/lunbo6.jpg"
   },
   {
     "revision": "c987d06107e10fdbfb1babcafa3438fb",
-    "url": "./static/media/lunbo7.c987d061.jpg"
+    "url": "./static/media/lunbo7.jpg"
   },
   {
     "revision": "652a7b5a70831f34df83bd71a55f26ed",
-    "url": "./static/media/tao.652a7b5a.png"
+    "url": "./static/media/tao.png"
   },
   {
     "revision": "e27d108d251bf3b1ae9700746b149765",
-    "url": "./static/media/yumao.e27d108d.jpg"
+    "url": "./static/media/yumao.jpg"
   }
 ]);
